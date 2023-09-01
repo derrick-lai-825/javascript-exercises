0e99e1c62a1ba50c6985c1a4531b7ed0655160a7
Derrick <derricklai825@gmail.com>
2023-09-01 00:20:43 -0700

    Exercise 2 completed

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,19 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    // Solution 1: Loop, but backwards
+    // let reversed = ''
+    // for (let i = string.length; i > 0; i--) {
+    //     reversed += string[i - 1]
+    // }
+
+    // return reversed
+
+    // Solution 2: Built in methods
+
+    // Split, reverse, and rejoin
+    let splitted = string.split("")
+    let reversed = splitted.reverse()
+    let joined = reversed.join("")
+    return joined
 
 };
 
